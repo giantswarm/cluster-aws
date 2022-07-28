47dd6ebe5907f00a8a1b939cd7848a415e3cb3d5
Marcus Noble <github@marcusnoble.co.uk>
2022-07-28 12:23:02 +0100

    Removed control plane replicas from values
    Signed-off-by: Marcus Noble <github@marcusnoble.co.uk>

---
+++ Makefile.custom.mk
@@ -0,0 +1,14 @@
+
+ensure-schema-gen:
+	@helm schema-gen --help &>/dev/null || helm plugin install https://github.com/mihaisee/helm-schema-gen.git
+
+.PHONY: schema-gen
+schema-gen: ensure-schema-gen ## Generates the values schema file
+	@cd helm/cluster-aws && helm schema-gen values.yaml > values.schema.json
+
+.PHONY: update-chart-deps
+update-chart-deps: ## Update chart dependencies to latest (matching) version
+	@cd helm/cluster-aws && \
+	sed -i.bk 's/version: \[\[ .Version \]\]/version: 1/' Chart.yaml && \
+	helm dependency update && \
+	mv Chart.yaml.bk Chart.yaml
