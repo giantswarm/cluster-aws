685b0b18f27eee1fafc7f182fffae395af5b09d7
Nick Jüttner <nick@giantswarm.io>
2022-10-12 14:34:20 +0200

    testing

--- helm/cluster-aws/files/opt/irsa-cloudfront.sh
+++ helm/cluster-aws/files/opt/irsa-cloudfront.sh
@@ -3,9 +3,4 @@
 # Replace Kubeadm Cloudfront placeholder
 CLOUDFRONT_DOMAIN=$(cat /etc/kubernetes/irsa/cloudfront-domain)
 
-kubeadm_file="/etc/kubeadm.yml"
-if [[ ! -f ${kubeadm_file} ]]; then
-    kubeadm_file="/run/kubeadm/kubeadm.yaml"
-fi
-
-sed -i "s/PLACEHOLDER_CLOUDFRONT_DOMAIN/$CLOUDFRONT_DOMAIN/g" $kubeadm_file
+sed -i "s/PLACEHOLDER_CLOUDFRONT_DOMAIN/$CLOUDFRONT_DOMAIN/g" /run/kubeadm/kubeadm.yaml
