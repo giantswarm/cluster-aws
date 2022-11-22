eb8df6ab8e3c4e69f6324f29acd916cfb451e206
Nick Jüttner <nick@juni.io>
2022-11-22 10:40:58 +0100

    Fix service account issuer
    We currently facing normalizing issuer errors when trying to use IRSA. This is because I missed the "https://" when setting the --service-account-issuer API flag, it needs to start with "https://" otherwise it's not working.

--- helm/cluster-aws/files/opt/irsa-cloudfront.sh
+++ helm/cluster-aws/files/opt/irsa-cloudfront.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Replace Kubeadm Cloudfront placeholder
-CLOUDFRONT_DOMAIN=$(cat /etc/kubernetes/irsa/cloudfront-domain)
+CLOUDFRONT_DOMAIN="https://$(cat /etc/kubernetes/irsa/cloudfront-domain)"
 
 sed -i "s/PLACEHOLDER_CLOUDFRONT_DOMAIN/$CLOUDFRONT_DOMAIN/g" /run/kubeadm/kubeadm.yaml
