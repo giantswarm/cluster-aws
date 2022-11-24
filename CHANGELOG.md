6c84c3d3c355918058f71e12900d8a88b92e03ca
Nick Jüttner <nick@juni.io>
2022-11-24 09:46:00 +0100

    fix sed command (#167)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Fixed
+
+- Change sed to fix replacement for Cloudfront placeholder.
+
 ### Changed
 
 - Make `baseDomain` a required value.
--- helm/cluster-aws/files/opt/irsa-cloudfront.sh
+++ helm/cluster-aws/files/opt/irsa-cloudfront.sh
@@ -3,4 +3,4 @@
 # Replace Kubeadm Cloudfront placeholder
 CLOUDFRONT_DOMAIN="https://$(cat /etc/kubernetes/irsa/cloudfront-domain)"
 
-sed -i "s/PLACEHOLDER_CLOUDFRONT_DOMAIN/$CLOUDFRONT_DOMAIN/g" /run/kubeadm/kubeadm.yaml
+sed -i "s|PLACEHOLDER_CLOUDFRONT_DOMAIN|$CLOUDFRONT_DOMAIN|g" /run/kubeadm/kubeadm.yaml
