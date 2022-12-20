882ac86d172028f0a18fd3422b2e074d6ff72e80
Berk Dehrioğlu <bdehrioglu@yahoo.com>
2022-12-20 15:51:47 +0300

    use aws ntp server as default

---
+++ helm/cluster-aws/files/opt/set-ntp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+AWS_NTP_SERVER="/etc/systemd/timesyncd.conf"
+
+NTP_SERVER="$(cat /etc/systemd/timeconf.d 2>/dev/null | grep 169.254.169.123)"
+
+if [ -z "$NTP_SERVER" ]
+then
+  echo "NTP server is not set, setting it to AWS NTP Server"
+  echo "NTP=169.254.169.123" >> /etc/systemd/timesyncd.conf
+fi
