51cf80e6cfe6b6128d8a023e7afe678de24be35a
Berk Dehrioğlu <bdehrioglu@yahoo.com>
2022-12-22 10:11:36 +0300

    switch to chrony

--- helm/cluster-aws/files/opt/set-aws-ntp.sh
+++ helm/cluster-aws/files/opt/set-aws-ntp.sh
@@ -7,6 +7,6 @@ NTP_SERVER="$(cat /etc/systemd/timeconf.d 2>/dev/null | grep 169.254.169.123)"
 if [ -z "$NTP_SERVER" ]
 then
   echo "NTP server is not set, setting it to AWS NTP Server"
-  echo "NTP=169.254.169.123" >> /etc/systemd/timesyncd.conf
-  systemctl restart systemd-timesyncd
+  echo "server 169.254.169.123 prefer iburst" >> /etc/chrony/chrony.conf
+  service chrony force-reload
 fi
