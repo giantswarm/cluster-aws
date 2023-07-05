78d9a79169f49dbcac44e2ca574e15dd44a48cbd
Puru <5674762+tuladhar@users.noreply.github.com>
2023-07-06 00:06:09 +0545

    Update install-teleport.sh

--- helm/cluster-aws/files/opt/install-teleport.sh
+++ helm/cluster-aws/files/opt/install-teleport.sh
@@ -2,8 +2,10 @@
 
 command -v teleport && exit 0
 
+TELEPORT_VERISON="13.1.5"
+TELEPORT_TARBALL="teleport-v${TELEPORT_VERISON}-linux-amd64-bin.tar.gz"
+
 cd /opt
 echo "Extracting $TELEPORT_TARBALL"
 tar zxf $TELEPORT_TARBALL
 teleport/install
-rm -vfr teleport
