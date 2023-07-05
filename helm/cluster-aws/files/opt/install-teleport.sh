391b46d7713e06772bdb9639c25880d32878203b
Puru <5674762+tuladhar@users.noreply.github.com>
2023-07-05 23:15:29 +0545

    Update install-teleport.sh

--- helm/cluster-aws/files/opt/install-teleport.sh
+++ helm/cluster-aws/files/opt/install-teleport.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+command -v teleport && exit 0
+
 TELEPORT_VERISON="13.1.5"
-command -v teleport || curl https://goteleport.com/static/install.sh | bash -s $TELEPORT_VERISON
+TELEPORT_TARBALL="teleport-v${TELEPORT_VERISON}-linux-amd64-bin.tar.gz"
+TELEPORT_URL="https://cdn.teleport.dev/$TELEPORT_TARBALL"
+
+cd /opt
+wget --show-progress $TELEPORT_URL
+echo "Extracting $TELEPORT_TARBALL"
+tar zxf $TELEPORT_TARBALL
+teleport/install
+rm -v $TELEPORT_TARBALL
