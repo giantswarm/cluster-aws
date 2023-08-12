14519f85feed3ef0493c615ecf9b1177c5cdd9f9
Puru <5674762+tuladhar@users.noreply.github.com>
2023-08-12 23:27:18 +0545

    Make it compatible with CAPV

--- helm/cluster-aws/files/opt/teleport-node-role.sh
+++ helm/cluster-aws/files/opt/teleport-node-role.sh
@@ -3,7 +3,7 @@
 # This script is executed by Teleport dynamic label configuration to assign the K8s role 
 # for the node (control-plane or worker)
 
-if [ -e "/etc/systemd/system/kubelet.service" ]; then
+if systemctl is-active --quiet kubelet.service; then
     if [ -e "/etc/kubernetes/manifests/kube-apiserver.yaml" ]; then
         echo "control-plane"
     else
