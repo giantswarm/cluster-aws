a3c20e8e58d4922efb9759a9a9899251f9204013
calvix <vaclav@giantswarm.io>
2023-11-21 15:32:38 +0100

    expose-shutdownGracePeriod

--- helm/cluster-aws/files/opt/kubelet-config.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sed -i "s|shutdownGracePeriod: .*|shutdownGracePeriod: 5m|g" "/var/lib/kubelet/config.yaml"
-sed -i "s|shutdownGracePeriodCriticalPods: .*|shutdownGracePeriodCriticalPods: 1m|g" "/var/lib/kubelet/config.yaml"
-systemctl restart kubelet
