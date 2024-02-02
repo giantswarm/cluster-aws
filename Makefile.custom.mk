b6690feaefa6899883d92424763c539b7b05b994
Nikola Prokopić <nikola@prokopic.rs>
2024-02-02 11:22:54 +0100

    Remove unnecessary architect brackets cleanup
    And use CI values to render templates locally

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -1,9 +1,6 @@
 .PHONY: template
 template: ## Output the rendered template yaml
-	@cd helm/cluster-aws && \
-		sed -i 's/version: \[/version: 1 #\[/' Chart.yaml && \
-		helm template . && \
-		sed -i 's/version: 1 #\[/version: \[/' Chart.yaml
+	@cd helm/cluster-aws && helm template . -f ci/ci-values.yaml
 
 ensure-schema-gen:
 	@helm schema-gen --help &>/dev/null || helm plugin install https://github.com/mihaisee/helm-schema-gen.git
@@ -14,7 +11,4 @@ schema-gen: ensure-schema-gen ## Generates the values schema file
 
 .PHONY: update-chart-deps
 update-chart-deps: ## Update chart dependencies to latest (matching) version
-	@cd helm/cluster-aws && \
-	sed -i.bk 's/version: \[\[ .Version \]\]/version: 1/' Chart.yaml && \
-	helm dependency update && \
-	mv Chart.yaml.bk Chart.yaml
+	@cd helm/cluster-aws && helm dependency update
